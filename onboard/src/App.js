c4f01dc2755cd145cd07699e57b7188535860dd7
elindesta <elin.desta@live.se>
2020-12-10 18:16:23 +0100

    first commit

---
+++ onboard/src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import "./App.css";
+import Home from "./components/home";
+import Info from "./components/info";
+import Rules from "./components/rules";
+import Forum from "./components/forum";
+import Faq from "./components/faq";
+import Contact from "./components/contact";
+
+
+export default function App() {
+  return (
+    <Router>
+      <div className="menu">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Hem</Link>
+
+            </li>
+            <li>
+              <Link to="info">Information</Link>
+            </li>
+            <li>
+              <Link to="/rules">Regler</Link>
+              
+            </li>
+            <li>
+              <Link to="/forum">Forum</Link>
+              
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+              
+            </li>
+            <li>
+              <Link to="/contact">Kontakta Oss</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/faq">
+            <Faq />
+          </Route>
+          <Route path="/forum">
+            <Forum />
+          </Route>
+          <Route path="/rules">
+            <Rules />
+          </Route>
+          <Route path="/info">
+            <Info />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ onboard/src/components/rules.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Rules(props) {
+    return <div>
+
+    </div>
+}
+
+export default Rules;
