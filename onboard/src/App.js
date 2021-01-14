d2846574cd853318a669f62b9ac195b072150c6a
phani192 <54536657+phani192@users.noreply.github.com>
2021-01-15 00:58:45 +0100

    Add files via upload
    firebase included

--- onboard/src/App.js
+++ onboard/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,66 +15,118 @@ import Faq from "./components/faq";
 import Contact from "./components/contact";
 import Logo from "./img/kyh-logo.png";
 import Courses from "./components/courses";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDA9ozpcEFArjsc2x_eC32RgX8zKcfxpWo",
+  authDomain: "onboard-purple.firebaseapp.com"
+})
 
-export default function App() {
-  return (
-    <Router>
-      <div className="menu">
-        <nav>
-          <ul>
-          <img alt="" src={Logo}></img>
-            <li>
-              <Link to="/">Hem</Link>
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 
-            </li>
-            <li>
-              <Link to="info">Information</Link>
-            </li>
-            <li>
-              <Link to="/rules">Regler</Link>
-              
-            </li>
-            <li>
-              <Link to="/forum">Forum</Link>
-              
-            </li>
-            <li>
-              <Link to="/faq">FAQ</Link>
-              
-            </li>
-            <li>
-              <Link to="/contact">Kontakta Oss</Link>
-            </li>
-          </ul>
-        </nav>
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
 
-        {/* A <Switch> looks through its children <Route>s and
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+
+          <div>
+            <Router>
+              <div className="menu">
+                <nav>
+                  <ul>
+                    <img alt="" src={Logo}></img>
+                    <li>
+                      <Link to="/">Hem</Link>
+
+                    </li>
+                    <li>
+                      <Link to="info">Information</Link>
+                    </li>
+                    <li>
+                      <Link to="/rules">Regler</Link>
+
+                    </li>
+                    <li>
+                      <Link to="/forum">Forum</Link>
+
+                    </li>
+                    <li>
+                      <Link to="/faq">FAQ</Link>
+
+                    </li>
+                    <li>
+                      <Link to="/contact">Kontakta Oss</Link>
+                    </li>
+                  </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-        <Route path="/courses">
-            <Courses />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/faq">
-            <Faq />
-          </Route>
-          <Route path="/forum">
-            <Forum />
-          </Route>
-          <Route path="/rules">
-            <Rules />
-          </Route>
-          <Route path="/info">
-            <Info />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+                <Switch>
+                  <Route path="/courses">
+                    <Courses />
+                  </Route>
+                  <Route path="/contact">
+                    <Contact />
+                  </Route>
+                  <Route path="/faq">
+                    <Faq />
+                  </Route>
+                  <Route path="/forum">
+                    <Forum />
+                  </Route>
+                  <Route path="/rules">
+                    <Rules />
+                  </Route>
+                  <Route path="/info">
+                    <Info />
+                  </Route>
+                  <Route path="/">
+                    <Home />
+                  </Route>
+                </Switch>
+              </div>
+            </Router>
+
+
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </div>
+
+        )
+          : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+
+          )
+
+        }
       </div>
-    </Router>
-  );
+    );
+  }
 }
+export default App;
