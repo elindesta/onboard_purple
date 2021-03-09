61a3e39adc77a9ea778359740c57acca680b905f
elindesta <elin.desta@live.se>
2021-03-09 18:22:23 +0100

    fixed errors

--- onboard/src/App.js
+++ onboard/src/App.js
@@ -7,14 +7,10 @@ import {
 } from "react-router-dom";
 
 import "./App.css";
-import Home from "./components/home";
 import Info from "./components/info";
-import Rules from "./components/rules";
-import Forum from "./components/forum";
 import Faq from "./components/faq";
 import Contact from "./components/contact";
 import Logo from "./img/kyh-logo.png";
-import Courses from "./components/courses";
 import SignIn from "./components/signIn";
 
 function LogIn() {
@@ -30,19 +26,7 @@ export default function App() {
         <img className="logo" alt="" src={Logo}></img>
           <ul>
             <li>
-              <Link to="/">Hem</Link>
-
-            </li>
-            <li>
               <Link to="info">Information</Link>
-            </li>
-            <li>
-              <Link to="/rules">Regler</Link>
-              
-            </li>
-            <li>
-              <Link to="/forum">Forum</Link>
-              
             </li>
             <li>
               <Link to="/faq">FAQ</Link>
@@ -60,29 +44,17 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/signin">
-            <SignIn />
-          </Route>
-        <Route path="/courses">
-            <Courses />
-          </Route>
           <Route path="/contact">
             <Contact />
           </Route>
           <Route path="/faq">
             <Faq />
           </Route>
-          <Route path="/forum">
-            <Forum />
-          </Route>
-          <Route path="/rules">
-            <Rules />
-          </Route>
           <Route path="/info">
             <Info />
           </Route>
           <Route path="/">
-            <Home />
+          <SignIn />
           </Route>
         </Switch>
       </div>
