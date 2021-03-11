b811c515a6513d6e54f6e4301d639c36c998b8f3
elindesta <elin.desta@live.se>
2021-03-11 19:20:33 +0100

    fixed errors and merged branch firebase-new

--- onboard/src/App.js
+++ onboard/src/App.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import React, { Component } from "react";
 import {
   BrowserRouter as Router,
@@ -11,53 +13,100 @@ import Info from "./components/info";
 import Faq from "./components/faq";
 import Contact from "./components/contact";
 import Logo from "./img/kyh-logo.png";
-import SignIn from "./components/signIn";
+// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function LogIn() {
-  window.location.href = "signin";
-}
+firebase.initializeApp({
+  apiKey: "AIzaSyDA9ozpcEFArjsc2x_eC32RgX8zKcfxpWo",
+  authDomain: "onboard-purple.firebaseapp.com"
+})
 
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 
-export default function App() {
-  return (
-    <Router>
-      <div className="menu">
-        <nav className="navBar">
-        <img className="logo" alt="" src={Logo}></img>
-          <ul>
-            <li>
-              <Link to="info">Information</Link>
-            </li>
-            <li>
-              <Link to="/faq">FAQ</Link>
-              
-            </li>
-            <li>
-              <Link to="/contact">Kontakta Oss</Link>
-            </li>
-            <li>
-              <button className="signInBtn" onClick={LogIn}>Logga In</button>
-            </li>
-          </ul>
-        </nav>
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
 
-        {/* A <Switch> looks through its children <Route>s and
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+
+          <div>
+            <Router>
+              <div className="menu">
+                <nav>
+                  <ul>
+                    <img alt="" src={Logo}></img>
+                    <li>
+                      <Link to="/">Hem</Link>
+
+                    </li>
+                    <li>
+                      <Link to="info">Information</Link>
+                    </li>
+                    <li>
+                      <Link to="/faq">FAQ</Link>
+
+                    </li>
+                    <li>
+                      <Link to="/contact">Kontakta Oss</Link>
+                    </li>
+                  </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/faq">
-            <Faq />
-          </Route>
-          <Route path="/info">
-            <Info />
-          </Route>
-          <Route path="/">
-          <SignIn />
-          </Route>
-        </Switch>
+                <Switch>
+                  <Route path="/contact">
+                    <Contact />
+                  </Route>
+                  <Route path="/faq">
+                    <Faq />
+                  </Route>
+                  <Route path="/info">
+                    <Info />
+                  </Route>
+                  <Route path="/">
+
+                  </Route>
+                </Switch>
+              </div>
+            </Router>
+
+
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </div>
+
+        )
+          : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+
+          )
+
+        }
       </div>
-    </Router>
-  );
+    );
+  }
 }
+export default App;
---
+++ onboard/src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase/app";
+
+import 'firebase/auth';
+import 'firebase/firestore';
+
+export const auth = firebase.auth();
+
+
+export default firebase;
